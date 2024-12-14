54fadd85a7f86caeb943f15442776d727895c574
Sree-Rangaraj89 <sreerangaraj1@gmail.com>
2024-12-14 06:06:02 -0800

    Add files via upload

---
+++ tc sc/activity1.py
@@ -0,0 +1,11 @@
+def printnumber(n):
+    iterations=0
+    print("The number entered by user is",n)
+    iterations+=1
+    print("Total iterations done by the code is", iterations,"\n")
+
+
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time taken by our code won't change")
